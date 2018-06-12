24136a6ac547b1539cf86c501d53b4c3829f9e33
Daniël Stekelenburg <d.j.stekelenburg@students.uu.nl>
2018-06-12 16:18:52 +0200

    Added script for converting .txt word embedding dataset files

---
+++ WorkflowPatternFinder/Gensim/DataSetConverter.py
@@ -0,0 +1,5 @@
+# Gensim
+from gensim.models import *
+
+model = KeyedVectors.load_word2vec_format(r"C:\Users\dst\Source\Repos\WorkflowPatternFinder\WorkflowPatternFinder\Gensim\datasets\combined-160.txt")
+model.save(r"C:\Users\dst\Source\Repos\WorkflowPatternFinder\WorkflowPatternFinder\Gensim\datasets\combined-160.bin")
