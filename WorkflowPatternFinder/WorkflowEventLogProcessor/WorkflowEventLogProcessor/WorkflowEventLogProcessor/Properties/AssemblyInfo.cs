e2029a935fbede8109ca7e9eaef9f97ff0f1bfea
Daniël Stekelenburg <d.j.stekelenburg@students.uu.nl>
2018-07-24 15:53:45 +0200

    Renamed out-dated references to workfloweventlogfixer

--- WorkflowPatternFinder/WorkflowEventLogProcessor/WorkflowEventLogProcessor/WorkflowEventLogProcessor/Properties/AssemblyInfo.cs
+++ WorkflowPatternFinder/WorkflowEventLogProcessor/WorkflowEventLogProcessor/WorkflowEventLogProcessor/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WorkflowEventLogFixer")]
+[assembly: AssemblyTitle("WorkflowEventLogProcessor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WorkflowEventLogFixer")]
+[assembly: AssemblyProduct("WorkflowEventLogProcessor")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
