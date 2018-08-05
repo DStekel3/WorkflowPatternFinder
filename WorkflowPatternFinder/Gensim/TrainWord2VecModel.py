c04a62c27b5c7f377a68f64237d351e32bd91399
Daniël Stekelenburg <d.j.stekelenburg@students.uu.nl>
2018-08-06 00:10:50 +0200

    Added tab for training your own word2vec model based on txt files

--- WorkflowPatternFinder/Gensim/TrainWord2VecModel.py
+++ WorkflowPatternFinder/Gensim/TrainWord2VecModel.py
@@ -12,7 +12,7 @@ import re
 from gensim.parsing.preprocessing import strip_short, strip_numeric, strip_punctuation
 from gensim.parsing.preprocessing import stem # applies stemming to text. This function is not used yet
 
-directory = r"C:\Thesis\Profit analyses\testmap\csv"
+directory = r"C:\Thesis\Profit analyses\txt"
 _window = 2
 _min_count = 5
 _epochs = 5
@@ -31,48 +31,14 @@ if len(args) >= 2:
     else:
       print(args[1]+ " is not a directory!")
 
-if len(args) == 5:
+if len(args) >= 4:
         _window = int(args[2])
         _min_count = int(args[3])
-        _epochs = int(args[4])
 
-pathlist = Path(directory).glob('*.csv')
+pathlist = Path(directory).glob('*.txt')
 
-raw_corpus = []
+corpus = []
 
-fileIndex = 1
-
-
-for pathObj in pathlist:
-    path = str(pathObj)
-    # print(path)
-    print("Reading file "+ str(fileIndex), end="\r")
-    fileIndex = fileIndex + 1
-    # from openpyxl import load_workbook
-    # wb = load_workbook(path, read_only=True)
-    # ws = wb[wb.sheetnames[0]]
-    
-    with open(path) as csvfile:
-        ws = csv.DictReader(csvfile, delimiter=';')
-        # print("Loading in csv took", round(time.time() - start, 3), "seconds.")
-        
-        # read all distinct sentences in the current file. We define a sentence as the combination of "taakomschrijving" and "actieomschrijving".
-        # iterate over the rows in the specific column
-        sentences = []
-        for row in ws:
-            # sys.stdout.write("reading line " + str(ws.line_num) + "\r")
-            try:
-                event = row['Event']
-                sentence = strip_short(strip_numeric(strip_punctuation(event)), 4)
-                if sentence.encode("utf-8") not in sentences:
-                    sentences.append(sentence.encode("utf-8")) 
-            except ValueError:
-                print("Could not obtain value of row", row)
-                break
-        # print("Extending the corpus took", round(time.time() - start, 3), "seconds.")
-        raw_corpus.extend(sentences)
-  
-# print("raw corpus:", raw_corpus)
 
 import nltk
 from nltk.corpus import stopwords
@@ -88,53 +54,34 @@ stoplist = set(stopwords.words('dutch'))
 # 'en', 'in', 'te', 'haar', 'zich', 'der', 'geen', 'uw', 'hij', 'wat', 'heb', 'op', 'maar', 
 # 'wezen', 'ik', 'zijn', 'zelf'}
 
-# Lowercase each document, split it by white space and filter out stopwords
-texts = [[word for word in document.lower().split() if word.decode('utf-8') not in stoplist and not any(char>126 for char in word.lower())]
-         for document in raw_corpus]
 
-distinctTexts = [list(x) for x in set(tuple(x) for x in texts)]
+for txtObj in pathlist:
+  with open(str(txtObj)) as infile:
+    for line in infile:
+        sentence = strip_short(strip_numeric(strip_punctuation(line.lower())), 3)
+        wordlist = sentence.split()
+        corpus.append([w for w in wordlist if w not in stoplist]) 
 
-import os.path
+print('number of sentences: '+str(len(corpus)))
 
-f = open(os.path.join(os.path.dirname(directory), "sentences.txt"), "w")
-
-for line in distinctTexts:
-  print(line)
-  elements = [word.decode('utf-8') for word in line]
-  elements.append(str(texts.count(line)))
-  f.write(' '.join(elements)+"\n")
-f.close()
-
-print('number of sentences: '+str(len(texts)))
-
-####
-#### Doc2Vec approach
-####
 
 import gensim
 from gensim import models
-from gensim.models import doc2vec
-from gensim.models.doc2vec import TaggedDocument
 
-print('tagging docs')
+from gensim.models import Word2Vec
 
-taggedDocs = []
-for idx, doc in enumerate(texts):
-  taggedDocs.append(TaggedDocument([x.decode() for x in doc], str(idx)))
-
-from gensim.models import Doc2Vec
-
-model = gensim.models.Doc2Vec(taggedDocs, window=_window, min_count=_min_count, epochs=_epochs, corpus_count = len(taggedDocs), workers = 8)
-print(model)
-
+print("training word2vec model...")
 start = time.time()
-model.train(taggedDocs, total_examples=model.corpus_count, epochs=model.epochs)
-print("Training the model took ", round(time.time() - start, 3), "seconds.")
-import os
+model = gensim.models.Word2Vec(corpus, window=_window, min_count=_min_count, workers = 8)
+print(model)
+print("Training took ", round(time.time() - start, 3), "seconds.")
 
-new_path = os.path.abspath(os.path.join(directory, os.pardir)) + "/trained.gz"
+import os
+new_path = os.path.abspath(os.path.join(directory, os.pardir)) + r"\trained.bin"
 model.save(os.path.abspath(new_path))
-print("Trained model and saved to ", new_path)
+print("Saved model to file: ", new_path)
+print("Press Enter to continue...")
+input()
 
 ####       
 #### Bag-of-words approach: Latent Semantic Analysis
